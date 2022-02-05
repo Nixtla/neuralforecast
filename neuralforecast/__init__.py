6470cd71287dfe744e03a9e5d90be313aa6b7ddf
FedericoGarza <fede.garza.ramirez@gmail.com>
2022-02-04 19:06:07 -0600

    feat: add notebook example and gitignore

--- neuralforecast/__init__.py
+++ neuralforecast/__init__.py
@@ -1,1 +1,32 @@
 __version__ = "0.0.2"
+
+import neuralforecast.data
+import neuralforecast.data.scalers
+import neuralforecast.data.tsdataset
+import neuralforecast.data.tsloader
+import neuralforecast.data.utils
+import neuralforecast.data.datasets.epf
+import neuralforecast.data.datasets.gefcom2012
+import neuralforecast.data.datasets.gefcom2014
+import neuralforecast.data.datasets.long_horizon
+import neuralforecast.data.datasets.m3
+import neuralforecast.data.datasets.m4
+import neuralforecast.data.datasets.m5
+import neuralforecast.data.datasets.tourism
+import neuralforecast.data.datasets.utils
+import neuralforecast.experiments
+import neuralforecast.experiments.utils
+import neuralforecast.losses
+import neuralforecast.losses.gw_test
+import neuralforecast.losses.numpy
+import neuralforecast.losses.pytorch
+import neuralforecast.losses.utils
+import neuralforecast.models
+import neuralforecast.models.esrnn.esrnn
+import neuralforecast.models.esrnn.mqesrnn
+import neuralforecast.models.nbeats.nbeats
+import neuralforecast.models.nhits.nhits
+import neuralforecast.models.transformer.autoformer
+import neuralforecast.models.transformer.informer
+import neuralforecast.models.transformer.transformer
+
