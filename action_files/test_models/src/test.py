c3aa13a4db9761371af469b09daa9d20639446fc
cchallu <cristiani.challu@gmail.com>
2023-08-28 13:21:22 -0400

    minimal example pandas

---
+++ action_files/test_models/src/test.py
@@ -0,0 +1,10 @@
+import fire
+import pandas as pd
+
+
+def main() -> None:
+    Y_df = pd.read_csv('https://datasets-nixtla.s3.amazonaws.com/M3-Monthly.csv', parse_dates=['ds']).head()
+    Y_df.to_csv('data/evaluation.csv')
+
+if __name__ == '__main__':
+    fire.Fire(main)
