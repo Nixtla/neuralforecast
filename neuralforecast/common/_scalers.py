Correct REVIN scaling order

neuralforecast/common/_scalers.py
@@ -425,11 +425,12 @@ class TemporalNorm(nn.Module):
         self.x_shift = x_shift
         self.x_scale = x_scale
 
-        if self.scaler_type == "revin":
-            x_shift = x_shift + self.revin_bias
-            x_scale = x_scale * self.revin_weight
-
         z = self.scaler(x, x_shift, x_scale)
+
+        if self.scaler_type == "revin":
+            z = self.revin_weight * z
+            z = z + self.revin_bias
+
         return z
 
     # @torch.no_grad()
@@ -442,10 +443,18 @@ class TemporalNorm(nn.Module):
         **Returns:**<br>
         `x`: torch.Tensor original data.
         """
+        if self.scaler_type == "revin":
+            z = z - self.revin_bias
+            z = z / (self.revin_weight + self.eps)
+
         if x_shift is None:
             x_shift = self.x_shift
         if x_scale is None:
             x_scale = self.x_scale
+
+        if self.scaler_type == "revin":
+            x_shift = x_shift - self.revin_bias
+            x_scale = x_scale / self.revin_weight
 
         x = self.inverse_scaler(z, x_shift, x_scale)
         return x