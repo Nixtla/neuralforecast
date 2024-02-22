e661312e56d20b5993bc38b705ab1934687a9215
Marco Peixeiro <marcopeix@outlook.com>
2024-02-22 18:58:07 +0000

    Bash script to run experiments on all GPUs

---
+++ experiments/nf_evaluation/gpu_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+datasets=("M4-quarterly" "M4-monthly" "M4-daily" "M4-hourly")
+
+models=("AutoLSTM" "AutoRNN" "AutoGRU" "AutoDilatedRNN" "AutoDeepAR" "AutoTCN" "AutoMLP" "AutoNBEATS" "AutoNHITS" "AutoDLinear" "AutoTFT" "AutoVanillaTransformer" "AutoInformer" "AutoAutoformer" "AutoFEDformer" "AutoTimesNet" "AutoPatchTST")
+
+for dataset in "${datasets[@]}"; do
+    for model in "${models[@]}"; do
+        CUDA_VISIBLE_DEVICES=1 python run_experiments.py --dataset "$dataset" --model "$model"
+    done
+done
