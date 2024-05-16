4da5aa41690eb726f393e6caa1954316a0ebba39
Olivier Sprangers <osprangers@gmail.com>
2024-05-16 17:27:56 +0200

    fix_union_in_function_signature

--- neuralforecast/models/moment.py
+++ neuralforecast/models/moment.py
@@ -4,7 +4,7 @@
 __all__ = ['MOMENT']
 
 # %% ../../nbs/models.moment.ipynb 6
-from typing import Optional
+from typing import Optional, Union
 
 import warnings
 import torch
@@ -110,110 +110,6 @@ class RevIN(nn.Module):
         return x
 
 # %% ../../nbs/models.moment.ipynb 11
-class PositionalEmbedding(nn.Module):
-    def __init__(self, d_model, max_len=5000, model_name="MOMENT"):
-        super(PositionalEmbedding, self).__init__()
-        self.model_name = model_name
-
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model).float()
-        pe.require_grad = False
-
-        position = torch.arange(0, max_len).float().unsqueeze(1)
-        div_term = (
-            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
-        ).exp()
-
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-
-        pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
-
-    def forward(self, x):
-        if (
-            self.model_name == "MOMENT"
-            or self.model_name == "TimesNet"
-            or self.model_name == "GPT4TS"
-        ):
-            return self.pe[:, : x.size(2)]
-        else:
-            return self.pe[:, : x.size(1)]
-
-
-class PatchEmbedding(nn.Module):
-    def __init__(
-        self,
-        d_model: int = 768,
-        seq_len: int = 512,
-        patch_len: int = 8,
-        stride: int = 8,
-        dropout: float = 0.1,
-        add_positional_embedding: bool = False,
-        value_embedding_bias: bool = False,
-        orth_gain: float = 1.41,
-    ):
-        super(PatchEmbedding, self).__init__()
-        self.patch_len = patch_len
-        self.seq_len = seq_len
-        self.stride = stride
-        self.d_model = d_model
-        self.add_positional_embedding = add_positional_embedding
-
-        self.value_embedding = nn.Linear(patch_len, d_model, bias=value_embedding_bias)
-        self.mask_embedding = nn.Parameter(torch.zeros(d_model))
-
-        if orth_gain is not None:
-            torch.nn.init.orthogonal_(self.value_embedding.weight, gain=orth_gain)
-            if value_embedding_bias:
-                self.value_embedding.bias.data.zero_()
-            # torch.nn.init.orthogonal_(self.mask_embedding, gain=orth_gain) # Fails
-
-        # Positional embedding
-        if self.add_positional_embedding:
-            self.position_embedding = PositionalEmbedding(d_model)
-
-        # Residual dropout
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
-        mask = Masking.convert_seq_to_patch_view(
-            mask, patch_len=self.patch_len
-        ).unsqueeze(-1)
-        # mask : [batch_size x n_patches x 1]
-        n_channels = x.shape[1]
-        mask = (
-            mask.repeat_interleave(self.d_model, dim=-1)
-            .unsqueeze(1)
-            .repeat(1, n_channels, 1, 1)
-        )
-        # mask : [batch_size x n_channels x n_patches x d_model]
-
-        # Input encoding
-        x = mask * self.value_embedding(x) + (1 - mask) * self.mask_embedding
-        if self.add_positional_embedding:
-            x = x + self.position_embedding(x)
-
-        return self.dropout(x)
-
-
-class Patching(nn.Module):
-    def __init__(self, patch_len: int, stride: int):
-        super().__init__()
-        self.patch_len = patch_len
-        self.stride = stride
-        if self.stride != self.patch_len:
-            warnings.warn(
-                "Stride and patch length are not equal. "
-                "This may lead to unexpected behavior."
-            )
-
-    def forward(self, x):
-        x = x.unfold(dimension=-1, size=self.patch_len, step=self.stride)
-        # x : [batch_size x n_channels x num_patch x patch_len]
-        return x
-
-# %% ../../nbs/models.moment.ipynb 13
 class Masking:
     def __init__(
         self, mask_ratio: float = 0.3, patch_len: int = 8, stride: Optional[int] = None
@@ -325,6 +221,110 @@ class Masking:
         mask = self._mask_patch_view(x, input_mask=input_mask)
         return self.convert_patch_to_seq_view(mask, self.patch_len).long()
 
+# %% ../../nbs/models.moment.ipynb 13
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_len=5000, model_name="MOMENT"):
+        super(PositionalEmbedding, self).__init__()
+        self.model_name = model_name
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        ).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        if (
+            self.model_name == "MOMENT"
+            or self.model_name == "TimesNet"
+            or self.model_name == "GPT4TS"
+        ):
+            return self.pe[:, : x.size(2)]
+        else:
+            return self.pe[:, : x.size(1)]
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(
+        self,
+        d_model: int = 768,
+        seq_len: int = 512,
+        patch_len: int = 8,
+        stride: int = 8,
+        dropout: float = 0.1,
+        add_positional_embedding: bool = False,
+        value_embedding_bias: bool = False,
+        orth_gain: float = 1.41,
+    ):
+        super(PatchEmbedding, self).__init__()
+        self.patch_len = patch_len
+        self.seq_len = seq_len
+        self.stride = stride
+        self.d_model = d_model
+        self.add_positional_embedding = add_positional_embedding
+
+        self.value_embedding = nn.Linear(patch_len, d_model, bias=value_embedding_bias)
+        self.mask_embedding = nn.Parameter(torch.zeros(d_model))
+
+        if orth_gain is not None:
+            torch.nn.init.orthogonal_(self.value_embedding.weight, gain=orth_gain)
+            if value_embedding_bias:
+                self.value_embedding.bias.data.zero_()
+            # torch.nn.init.orthogonal_(self.mask_embedding, gain=orth_gain) # Fails
+
+        # Positional embedding
+        if self.add_positional_embedding:
+            self.position_embedding = PositionalEmbedding(d_model)
+
+        # Residual dropout
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        mask = Masking.convert_seq_to_patch_view(
+            mask, patch_len=self.patch_len
+        ).unsqueeze(-1)
+        # mask : [batch_size x n_patches x 1]
+        n_channels = x.shape[1]
+        mask = (
+            mask.repeat_interleave(self.d_model, dim=-1)
+            .unsqueeze(1)
+            .repeat(1, n_channels, 1, 1)
+        )
+        # mask : [batch_size x n_channels x n_patches x d_model]
+
+        # Input encoding
+        x = mask * self.value_embedding(x) + (1 - mask) * self.mask_embedding
+        if self.add_positional_embedding:
+            x = x + self.position_embedding(x)
+
+        return self.dropout(x)
+
+
+class Patching(nn.Module):
+    def __init__(self, patch_len: int, stride: int):
+        super().__init__()
+        self.patch_len = patch_len
+        self.stride = stride
+        if self.stride != self.patch_len:
+            warnings.warn(
+                "Stride and patch length are not equal. "
+                "This may lead to unexpected behavior."
+            )
+
+    def forward(self, x):
+        x = x.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        # x : [batch_size x n_channels x num_patch x patch_len]
+        return x
+
 # %% ../../nbs/models.moment.ipynb 15
 class NamespaceWithDefaults(Namespace):
     @classmethod
@@ -407,7 +407,7 @@ class ForecastingHead(nn.Module):
 
 
 class MOMENT_module(nn.Module):
-    def __init__(self, config: Namespace | dict, **kwargs: dict):
+    def __init__(self, config: Union[Namespace, dict], **kwargs: dict):
         super().__init__()
         config = self._update_inputs(config, **kwargs)
         config = self._validate_inputs(config)
@@ -437,7 +437,7 @@ class MOMENT_module(nn.Module):
         self.head = self._get_head(self.task_name)
 
     def _update_inputs(
-        self, config: Namespace | dict, **kwargs: dict
+        self, config: Union[Namespace, dict], **kwargs: dict
     ) -> NamespaceWithDefaults:
         if isinstance(config, dict) and "model_kwargs" in kwargs:
             return NamespaceWithDefaults(**{**config, **kwargs["model_kwargs"]})
@@ -616,7 +616,7 @@ class MOMENT_module(nn.Module):
 
 
 class MOMENTPipeline(MOMENT_module, PyTorchModelHubMixin):
-    def __init__(self, config: Namespace | dict, **kwargs: dict):
+    def __init__(self, config: Union[Namespace, dict], **kwargs: dict):
         self._validate_model_kwargs(**kwargs)
         self.new_task_name = kwargs.get("model_kwargs", {}).pop(
             "task_name", TASKS.RECONSTRUCTION
