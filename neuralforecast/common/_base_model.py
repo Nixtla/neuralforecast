f6d5a906372ba407f20972b477c9808f54a7f4c4
José Morales <jmoralz92@gmail.com>
2024-06-10 15:46:32 -0600

    move distributed logic to separate methods and bug fixes

--- neuralforecast/common/_base_model.py
+++ neuralforecast/common/_base_model.py
@@ -430,5 +430,8 @@ class BaseModel(pl.LightningModule):
             content = torch.load(f, **kwargs)
         with _disable_torch_init():
             model = cls(**content["hyper_parameters"])
-        model.load_state_dict(content["state_dict"], strict=True, assign=True)
+        if "assign" in inspect.signature(model.load_state_dict).parameters:
+            model.load_state_dict(content["state_dict"], strict=True, assign=True)
+        else:  # pytorch<2.1
+            model.load_state_dict(content["state_dict"], strict=True)
         return model
