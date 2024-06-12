d846004121a9105be70487e706c2aac52c2f212e
marcopeix <marco@nixtla.io>
2024-06-12 10:44:56 -0400

    Add AutoVCformer

--- neuralforecast/auto.py
+++ neuralforecast/auto.py
@@ -4,7 +4,7 @@
 __all__ = ['AutoRNN', 'AutoLSTM', 'AutoGRU', 'AutoTCN', 'AutoDeepAR', 'AutoDilatedRNN', 'AutoBiTCN', 'AutoMLP', 'AutoNBEATS',
            'AutoNBEATSx', 'AutoNHITS', 'AutoDLinear', 'AutoNLinear', 'AutoTiDE', 'AutoDeepNPTS', 'AutoTFT',
            'AutoVanillaTransformer', 'AutoInformer', 'AutoAutoformer', 'AutoFEDformer', 'AutoPatchTST',
-           'AutoiTransformer', 'AutoTimesNet', 'AutoStemGNN', 'AutoHINT', 'AutoTSMixer', 'AutoTSMixerx',
+           'AutoiTransformer', 'AutoVCformer', 'AutoTimesNet', 'AutoStemGNN', 'AutoHINT', 'AutoTSMixer', 'AutoTSMixerx',
            'AutoMLPMultivariate']
 
 # %% ../nbs/models.ipynb 2
@@ -42,6 +42,7 @@ from .models.fedformer import FEDformer
 from .models.patchtst import PatchTST
 from .models.timesnet import TimesNet
 from .models.itransformer import iTransformer
+from .models.vcformer import VCformer
 
 from .models.stemgnn import StemGNN
 from .models.hint import HINT
@@ -1598,7 +1599,93 @@ class AutoiTransformer(BaseAuto):
 
         return config
 
-# %% ../nbs/models.ipynb 104
+# %% ../nbs/models.ipynb 103
+class AutoVCformer(BaseAuto):
+
+    default_config = {
+        "input_size_multiplier": [1, 2, 3, 4, 5],
+        "h": None,
+        "n_series": None,
+        "hidden_size": tune.choice([64, 128, 256]),
+        "n_heads": tune.choice([4, 8]),
+        "hidden_ktd_layers": tune.choice([1, 2, 4]),
+        "learning_rate": tune.loguniform(1e-4, 1e-1),
+        "scaler_type": tune.choice([None, "robust", "standard"]),
+        "max_steps": tune.choice([500, 1000, 2000]),
+        "batch_size": tune.choice([32, 64, 128, 256]),
+        "loss": None,
+        "random_seed": tune.randint(1, 20),
+    }
+
+    def __init__(
+        self,
+        h,
+        n_series,
+        loss=MAE(),
+        valid_loss=None,
+        config=None,
+        search_alg=BasicVariantGenerator(random_state=1),
+        num_samples=10,
+        refit_with_val=False,
+        cpus=cpu_count(),
+        gpus=torch.cuda.device_count(),
+        verbose=False,
+        alias=None,
+        backend="ray",
+        callbacks=None,
+    ):
+
+        # Define search space, input/output sizes
+        if config is None:
+            config = self.get_default_config(h=h, backend=backend, n_series=n_series)
+
+        # Always use n_series from parameters, raise exception with Optuna because we can't enforce it
+        if backend == "ray":
+            config["n_series"] = n_series
+        elif backend == "optuna":
+            mock_trial = MockTrial()
+            if (
+                "n_series" in config(mock_trial)
+                and config(mock_trial)["n_series"] != n_series
+            ) or ("n_series" not in config(mock_trial)):
+                raise Exception(f"config needs 'n_series': {n_series}")
+
+        super(AutoVCformer, self).__init__(
+            cls_model=VCformer,
+            h=h,
+            loss=loss,
+            valid_loss=valid_loss,
+            config=config,
+            search_alg=search_alg,
+            num_samples=num_samples,
+            refit_with_val=refit_with_val,
+            cpus=cpus,
+            gpus=gpus,
+            verbose=verbose,
+            alias=alias,
+            backend=backend,
+            callbacks=callbacks,
+        )
+
+    @classmethod
+    def get_default_config(cls, h, backend, n_series):
+        config = cls.default_config.copy()
+        config["input_size"] = tune.choice(
+            [h * x for x in config["input_size_multiplier"]]
+        )
+
+        # Rolling windows with step_size=1 or step_size=h
+        # See `BaseWindows` and `BaseRNN`'s create_windows
+        config["step_size"] = tune.choice([1, h])
+        del config["input_size_multiplier"]
+        if backend == "optuna":
+            # Always use n_series from parameters
+            config["n_series"] = n_series
+            config = cls._ray_config_to_optuna(config)
+
+        return config
+
+# %% ../nbs/models.ipynb 108
 class AutoTimesNet(BaseAuto):
 
     default_config = {
@@ -1666,7 +1753,7 @@ class AutoTimesNet(BaseAuto):
 
         return config
 
-# %% ../nbs/models.ipynb 109
+# %% ../nbs/models.ipynb 113
 class AutoStemGNN(BaseAuto):
 
     default_config = {
@@ -1751,7 +1838,7 @@ class AutoStemGNN(BaseAuto):
 
         return config
 
-# %% ../nbs/models.ipynb 113
+# %% ../nbs/models.ipynb 117
 class AutoHINT(BaseAuto):
 
     def __init__(
@@ -1823,7 +1910,7 @@ class AutoHINT(BaseAuto):
     def get_default_config(cls, h, backend, n_series=None):
         raise Exception("AutoHINT has no default configuration.")
 
-# %% ../nbs/models.ipynb 118
+# %% ../nbs/models.ipynb 122
 class AutoTSMixer(BaseAuto):
 
     default_config = {
@@ -1909,7 +1996,7 @@ class AutoTSMixer(BaseAuto):
 
         return config
 
-# %% ../nbs/models.ipynb 122
+# %% ../nbs/models.ipynb 126
 class AutoTSMixerx(BaseAuto):
 
     default_config = {
@@ -1995,7 +2082,7 @@ class AutoTSMixerx(BaseAuto):
 
         return config
 
-# %% ../nbs/models.ipynb 126
+# %% ../nbs/models.ipynb 130
 class AutoMLPMultivariate(BaseAuto):
 
     default_config = {
