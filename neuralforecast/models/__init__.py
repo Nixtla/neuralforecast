14bd9e9cd547c05e382f8bbffd319b687022794e
marcopeix <marco@nixtla.io>
2024-09-08 11:47:59 -0400

    WIP - Allows import of RMoK

--- neuralforecast/models/__init__.py
+++ neuralforecast/models/__init__.py
@@ -2,7 +2,7 @@ __all__ = ['RNN', 'GRU', 'LSTM', 'TCN', 'DeepAR', 'DilatedRNN',
            'MLP', 'NHITS', 'NBEATS', 'NBEATSx', 'DLinear', 'NLinear',
            'TFT', 'VanillaTransformer', 'Informer', 'Autoformer', 'PatchTST', 'FEDformer',
            'StemGNN', 'HINT', 'TimesNet', 'TimeLLM', 'TSMixer', 'TSMixerx', 'MLPMultivariate',
-           'iTransformer', 'BiTCN', 'TiDE', 'DeepNPTS', 'SOFTS', 'TimeMixer', 'KAN'
+           'iTransformer', 'BiTCN', 'TiDE', 'DeepNPTS', 'SOFTS', 'TimeMixer', 'KAN', 'RMoK',
            ]
 
 from .rnn import RNN
@@ -37,3 +37,4 @@ from .deepnpts import DeepNPTS
 from .softs import SOFTS
 from .timemixer import TimeMixer
 from .kan import KAN
+from .rmok import RMoK
