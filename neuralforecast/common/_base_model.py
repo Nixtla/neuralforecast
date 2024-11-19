b2c7691da969550ba94f517632329f55c0141b96
Olivier Sprangers <osprangers@gmail.com>
2024-11-19 23:18:33 +0100

    fix_base_model

--- neuralforecast/common/_base_model.py
+++ neuralforecast/common/_base_model.py
@@ -111,6 +111,7 @@ class BaseModel(pl.LightningModule):
         optimizer_kwargs: Union[Dict, None] = None,
         lr_scheduler: Union[torch.optim.lr_scheduler.LRScheduler, None] = None,
         lr_scheduler_kwargs: Union[Dict, None] = None,
+        dataloader_kwargs=None,
         **trainer_kwargs,
     ):
         super().__init__()
@@ -352,6 +353,7 @@ class BaseModel(pl.LightningModule):
 
         # DataModule arguments
         self.num_workers_loader = num_workers_loader
+        self.dataloader_kwargs = dataloader_kwargs
         self.drop_last_loader = drop_last_loader
         # used by on_validation_epoch_end hook
         self.validation_step_outputs: List = []
