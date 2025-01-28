5fb633193c42bcbc69bba74c68907241a1234247
t-minus <gohjingqiang@gmail.com>
2025-01-28 14:51:13 +0000

    Revert "Add arguments and doc string"
    This reverts commit c5f20a17cf0aa1b7a42164486d2a83e5b0561bb1.

--- neuralforecast/models/timesnet.py
+++ neuralforecast/models/timesnet.py
@@ -182,9 +182,6 @@ class TimesNet(BaseWindows):
         If True `TimeSeriesDataLoader` drops last non-full batch.
     `dataloader_kwargs`: dict, optional (default=None)
         List of parameters passed into the PyTorch Lightning dataloader by the `TimeSeriesDataLoader`. <br>
-    `config_optimizers`: <class 'function'>, optional, A callable function that implements the optimization behavior as detailed in <br>
-     https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.core.LightningModule.html#lightning.pytorch.core.LightningModule.configure_optimizers <br>
-     Note that the function must accept an argument which is the subclass of Neuralforecast's `BaseModel` to speficy the model's parameters() for the optimizer. <br>
     **trainer_kwargs
         Keyword trainer arguments inherited from [PyTorch Lighning's trainer](https://pytorch-lightning.readthedocs.io/en/stable/api/pytorch_lightning.trainer.trainer.Trainer.html?highlight=trainer)
 
@@ -230,7 +227,6 @@ class TimesNet(BaseWindows):
         random_seed: int = 1,
         drop_last_loader: bool = False,
         dataloader_kwargs=None,
-        config_optimizers=None,
         **trainer_kwargs
     ):
         super(TimesNet, self).__init__(
@@ -257,7 +253,6 @@ class TimesNet(BaseWindows):
             drop_last_loader=drop_last_loader,
             random_seed=random_seed,
             dataloader_kwargs=dataloader_kwargs,
-            config_optimizers=config_optimizers,
             **trainer_kwargs
         )
 
