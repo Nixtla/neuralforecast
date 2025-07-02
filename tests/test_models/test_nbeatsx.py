a026a3e43a7cfdf61dcbdaee216ebf751d967797
deven367 <masterdeven@gmail.com>
2025-07-02 17:10:46 -0400

    refactored tests for models and their respective AutoModels

--- tests/test_models/test_nbeatsx.py
+++ tests/test_models/test_nbeatsx.py
@@ -1,18 +1,35 @@
+from test_helpers import check_args
 
-
-import logging
-import os
-import warnings
-
+from neuralforecast.auto import AutoNBEATSx, NBEATSx
+from neuralforecast.common._base_auto import MockTrial
 from neuralforecast.common._model_checks import check_model
-from neuralforecast.models import NBEATSx
-
-os.environ["PYTORCH_ENABLE_MPS_FALLBACK"]="1"
 
 
-def test_nbeatsx_model():
-    logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
-    logging.getLogger("lightning_fabric").setLevel(logging.ERROR)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        check_model(NBEATSx, ["airpassengers"])
+def test_nbeatsx_model(suppress_warnings):
+    check_model(NBEATSx, ["airpassengers"])
+
+def test_autonbeatx(setup_dataset):
+    dataset = setup_dataset
+
+    # Unit test to test that Auto* model contains all required arguments from BaseAuto
+    check_args(AutoNBEATSx, exclude_args=['cls_model'])
+
+    # Unit test for situation: Optuna with updated default config
+    my_config = AutoNBEATSx.get_default_config(h=12, backend='optuna')
+    def my_config_new(trial):
+        config = {**my_config(trial)}
+        config.update({'max_steps': 2, 'val_check_steps': 1, 'input_size': 12, 'mlp_units': 3 * [[8, 8]]})
+        return config
+
+    model = AutoNBEATSx(h=12, config=my_config_new, backend='optuna', num_samples=1, cpus=1)
+    assert model.config(MockTrial())['h'] == 12
+    model.fit(dataset=dataset)
+
+    # Unit test for situation: Ray with updated default config
+    my_config = AutoNBEATSx.get_default_config(h=12, backend='ray')
+    my_config['max_steps'] = 2
+    my_config['val_check_steps'] = 1
+    my_config['input_size'] = 12
+    my_config['mlp_units'] = 3 * [[8, 8]]
+    model = AutoNBEATSx(h=12, config=my_config, backend='ray', num_samples=1, cpus=1)
+    model.fit(dataset=dataset)
--- tests/test_models/test_timexer.py
+++ tests/test_models/test_timexer.py
@@ -1,13 +1,35 @@
-import logging
-import warnings
+from test_helpers import check_args
 
+from neuralforecast.auto import AutoTimeXer, TimeXer
+from neuralforecast.common._base_auto import MockTrial
 from neuralforecast.common._model_checks import check_model
-from neuralforecast.models import TimeXer
 
 
-def test_timexer():
-    logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
-    logging.getLogger("lightning_fabric").setLevel(logging.ERROR)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        check_model(TimeXer, ["airpassengers"])
+def test_timexer(suppress_warnings):
+    check_model(TimeXer, ["airpassengers"])
+
+
+def test_autotimxer(setup_dataset):
+
+    # Unit test to test that Auto* model contains all required arguments from BaseAuto
+    check_args(AutoTimeXer, exclude_args=['cls_model'])
+
+    # Unit test for situation: Optuna with updated default config
+    my_config = AutoTimeXer.get_default_config(h=12, n_series=1, backend='optuna')
+    def my_config_new(trial):
+        config = {**my_config(trial)}
+        config.update({'max_steps': 1, 'val_check_steps': 1, 'input_size': 12, 'patch_len': 12})
+        return config
+
+    model = AutoTimeXer(h=12, n_series=1, config=my_config_new, backend='optuna', num_samples=1, cpus=1)
+    assert model.config(MockTrial())['h'] == 12
+    model.fit(dataset=setup_dataset)
+
+    # Unit test for situation: Ray with updated default config
+    my_config = AutoTimeXer.get_default_config(h=12, n_series=1, backend='ray')
+    my_config['max_steps'] = 1
+    my_config['val_check_steps'] = 1
+    my_config['input_size'] = 12
+    my_config['patch_len'] = 12
+    model = AutoTimeXer(h=12, n_series=1, config=my_config, backend='ray', num_samples=1, cpus=1)
+    model.fit(dataset=setup_dataset)
