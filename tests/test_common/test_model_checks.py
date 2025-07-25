367fc1e1e0f09cdb23127e15dcc30c90741a0753
deven367 <masterdeven@gmail.com>
2025-07-25 13:20:37 -0400

    use model imports from `MODEL_FILENAME_DICT`

--- tests/test_common/test_model_checks.py
+++ tests/test_common/test_model_checks.py
@@ -1,83 +1,9 @@
-import logging
-import warnings
-
 import pytest
 
 from neuralforecast.common._model_checks import check_model
-from neuralforecast.models import (
-    GRU,
-    KAN,
-    LSTM,
-    MLP,
-    NBEATS,
-    NHITS,
-    RNN,
-    SOFTS,
-    TCN,
-    TFT,
-    Autoformer,
-    BiTCN,
-    DeepAR,
-    DeepNPTS,
-    DilatedRNN,
-    DLinear,
-    FEDformer,
-    Informer,
-    MLPMultivariate,
-    NBEATSx,
-    NLinear,
-    RMoK,
-    StemGNN,
-    TiDE,
-    TimeMixer,
-    TimesNet,
-    TSMixer,
-    TSMixerx,
-    VanillaTransformer,
-    iTransformer,
-)
+from neuralforecast.core import MODEL_FILENAME_DICT
 
 
-@pytest.fixture
-def setup_module():
-    models = [
-        RNN,
-        GRU,
-        TCN,
-        LSTM,
-        DeepAR,
-        DilatedRNN,
-        BiTCN,
-        MLP,
-        NBEATS,
-        NBEATSx,
-        NHITS,
-        DLinear,
-        NLinear,
-        TiDE,
-        DeepNPTS,
-        TFT,
-        VanillaTransformer,
-        Informer,
-        Autoformer,
-        FEDformer,
-        TimesNet,
-        iTransformer,
-        KAN,
-        RMoK,
-        StemGNN,
-        TSMixer,
-        TSMixerx,
-        MLPMultivariate,
-        SOFTS,
-        TimeMixer,
-    ]
-    return models
-
-def test_model_checks(setup_module):
-    logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
-    logging.getLogger("lightning_fabric").setLevel(logging.ERROR)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        for model in setup_module:
-            check_model(model, checks=["losses"])
+@pytest.mark.parametrize("model", [v for k,v in MODEL_FILENAME_DICT.items() if 'auto' not in k])
+def test_model_checks(setup_test_environment, suppress_warnings, model):
+    check_model(model, checks=["losses"])
