c4a0488f9ac3d5a982ecc305fe0f34fe7e670915
JQGoh <jq@nixtla.io>
2025-09-12 07:36:28 +0000

    StrEnum not supported before python3.11

--- neuralforecast/common/enums.py
+++ neuralforecast/common/enums.py
@@ -1,6 +1,6 @@
-from enum import StrEnum
+from enum import Enum
 
-class TimeSeriesDatasetEnum(StrEnum):
+class TimeSeriesDatasetEnum(str, Enum):
     Datetime = "ds"
     UniqueId = "unique_id"
     Target = "y"
