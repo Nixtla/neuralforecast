6933c753ec5a78dd696e81e9a9a61fe294c6c9bb
marcopeix <marco@nixtla.io>
2026-03-26 14:36:33 -0400

    Move versioning to pyproject.toml

--- neuralforecast/__init__.py
+++ neuralforecast/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "3.1.6"
+from importlib.metadata import version
+
+__version__ = version("neuralforecast")
 __all__ = ['NeuralForecast']
 from .common._base_model import DistributedConfig  # noqa: F401
 from .core import NeuralForecast
